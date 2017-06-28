fdd0e0b1a30c7728e69db285839a8837479bf5ed
kai <kai@DESKTOP-JO8CKD7>
2017-06-28 09:31:29 +0200

    Add project files.

---
+++ Dwx17.Clients.AndroidApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Dwx17.Clients.AndroidApp
+{
+    [Activity(Label = "Dwx17.Clients.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
